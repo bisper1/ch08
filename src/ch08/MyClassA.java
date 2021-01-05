92af2b770a682fe3b1d48ee0919f335a0dd7cba3
bisper1 <bisper@naver.com>
2021-01-05 16:40:08 +0900

    MyInterface를 상속받아 구현한 MyClassA, MyClassB 클래스.

---
+++ src/ch08/MyClassA.java
@@ -0,0 +1,11 @@
+package ch08;
+
+// 다른 회사의 클래스 / 수정이 불가능함
+public class MyClassA implements MyInterface {
+
+	@Override
+	public void method1() {
+		System.out.println("MyClassA의 method1 실행");
+	}
+
+}
