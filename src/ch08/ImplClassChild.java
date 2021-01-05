49627b3cffec61add3bd613a93ca17d49b46a5f0
bisper1 <bisper@naver.com>
2021-01-05 10:13:17 +0900

    추상 클래스 ImplClass를 상속받아 method3를 구현

---
+++ src/ch08/ImplClassChild.java
@@ -0,0 +1,11 @@
+package ch08;
+
+public class ImplClassChild extends ImplClass {
+
+	@Override
+	public void method3() {
+		// TODO Auto-generated method stub
+		System.out.println("ImplClass 에서 상속받은 추상 메서드 method3 구현");
+	}
+
+}
